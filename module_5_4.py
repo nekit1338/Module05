f31cdb939000429f31a020529d8cb5ee74bb90aa
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-08-12 03:32:48 +0500

    Update module_5_4.py

--- module_5_4.py
+++ module_5_4.py
@@ -9,53 +9,6 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def __len__(self):
-        return self.number_of_floors
-
-    def __str__(self):
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
-
-    def __eq__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors == other.number_of_floors
-
-    def __lt__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors < other.number_of_floors
-
-    def __le__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors <= other.number_of_floors
-
-    def __gt__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors > other.number_of_floors
-
-    def __ge__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors >= other.number_of_floors
-
-    def __ne__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors != other.number_of_floors
-
-    def __add__(self, value):
-        self.number_of_floors = self.number_of_floors + value
-        return self
-
-    def __radd__(self, value):
-        return self.__add__(value)
-
-    def __iadd__(self, value):
-        self.number_of_floors += value
-        return self
-
-    def go_to(self, new_floor: int):
-        if 1 < new_floor <= self.number_of_floors:
-            print(f"{new_floor}")
-        else:
-            print("Такого этажа не существует")
-
     def __del__(self):
         print(f"{self.name} снесён, но он останется в истории")
 
